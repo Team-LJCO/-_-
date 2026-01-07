633cfb9341135358b986e8da88af787fe2a3de98
오민석 <ominsuk@MinMacBook-Air.local>
2026-01-07 23:22:54 +0900

    refactor(entity): JPA 어노테이션 제거 및 Lombok 적용

--- Team_LJCO_back/src/main/java/com/korit/team_ljco/entity/Category.java
+++ Team_LJCO_back/src/main/java/com/korit/team_ljco/entity/Category.java
@@ -1,33 +1,17 @@
 package com.korit.team_ljco.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Getter
+@Data
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "categories") // DB 테이블 이름
 public class Category {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "category_id")
     private Long categoryId;
-
-    @Column(name = "name", nullable = false, unique = true)
     private String name;
-
-    @Column(name = "emoji")
     private String emoji;
-
-    @Column(name = "description")
     private String description;
-
-    // 생성자 (필요시 사용)
-    public Category(String name, String emoji, String description) {
-        this.name = name;
-        this.emoji = emoji;
-        this.description = description;
-    }
 }
